6d1ad19a785d546280423b3bb8d131bb99337325
NickR23 <nic.rr@icloud.com>
2025-05-27 15:08:41 -0400

    Fix main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
-#include "hash-object.h"
+
+#include <types.h>
+#include <hash-object.h>
 
 using namespace std;
 int main(){
-    std::string path = "../test/assets/bee_movie.txt";
-    std::string r = hash_object(path);
+    std::string objData = "hello";
+    std::vector<uint8_t> obj(objData.begin(), objData.end());
+    std::string r = nit::hashBlob(obj);
     cout << r << endl;
     return 0;
 }
