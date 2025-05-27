38543b6be28c32f9fb025a68eb291c8a88a14fcd
NickR23 <nic.rr@icloud.com>
2025-05-27 14:32:04 -0400

    Add FilesystemAdaptor implementation

--- src/FileSystemAdaptor.h
+++ src/FileSystemAdaptor.h
@@ -3,19 +3,38 @@
  */
 #include <vector>
 #include <filesystem>
+#include <fstream>
 
 #include <types.h>
 
 class FileSystemAdaptorInterface {
-    virtual nit::Blob writeFile(const std::filesystem::path& path) = 0;
-    virtual void putObject(nit::Blob object) = 0;
+    virtual void writeBlobToFile(const std::filesystem::path& path, const nit::Blob& blob) = 0;
+    virtual nit::Blob getBlobFromFile(const std::filesystem::path& path) = 0;
 };
 
 class FileSystemAdaptorImpl : FileSystemAdaptorInterface {
-    nit::Blob writeFile(const std::filesystem::path& path) {
-        return {};
+    void writeBlobToFile(const std::filesystem::path& path, const nit::Blob& blob) {
+        std::ofstream out(path, std::ios::binary);
+        if (!out) {
+            throw std::runtime_error("Failed to open file for write: " + path.string());
+        }
+        out.write(reinterpret_cast<const char*>(blob.data()), blob.size());
     }
 
-    void putObject(nit::Blob object) {
+    nit::Blob getBlobFromFile(const std::filesystem::path& path) {
+       std::ifstream in(path, std::ios::binary);
+       if (!in) {
+            throw std::runtime_error("Failed to open file for read: " + path.string());
+       }
+
+       std::streamsize size = in.tellg(); // Gets the file size in bytes
+       in.seekg(0, std::ios::beg);
+
+       std::vector<uint8_t> buffer(size);
+       if(!in.read(reinterpret_cast<char*>(buffer.data()), size)) {
+            throw std::runtime_error("Failed to read file: " + path.string());
+       }
+
+       return buffer;
     }
 };
