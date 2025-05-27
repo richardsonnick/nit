f170c83f192fab9ef3ec809d131eea3a2309d83c
NickR23 <nic.rr@icloud.com>
2025-05-27 15:08:54 -0400

    Fix hash object test

--- test/hash_object_test.cpp
+++ test/hash_object_test.cpp
@@ -5,6 +5,8 @@
 TEST(SHA1Tests, HashFile) {
     const std::string current_dir = std::filesystem::path(__FILE__).parent_path().string();
     const std::string bee_movie_path = current_dir + "/assets/bee_movie.txt";
-    auto r = hash_object(bee_movie_path);
+    FileSystemAdaptorImpl fs;
+    nit::Blob blob = fs.getBlobFromFile(bee_movie_path);
+    auto r = nit::hashBlob(blob);
     EXPECT_EQ(r, "93ae3d6436613af8a6957db81e1701fbc50de7a8");
 }
