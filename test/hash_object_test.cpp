24ac2298cc60ec92f321a1ca2a0c7ba6c01455ed
NickR23 <nic.rr@icloud.com>
2025-05-27 15:38:56 -0400

    Add hash-object

--- test/hash_object_test.cpp
+++ test/hash_object_test.cpp
@@ -1,12 +1,38 @@
 #include <gtest/gtest.h>
-#include <hash-object.h>
 #include <filesystem>
 
-TEST(SHA1Tests, HashFile) {
+#include <hash-object.h>
+#include <MockFileSystemAdaptor.h>
+
+TEST(ObjectHashTests, TestHashBlob) {
     const std::string current_dir = std::filesystem::path(__FILE__).parent_path().string();
     const std::string bee_movie_path = current_dir + "/assets/bee_movie.txt";
     FileSystemAdaptorImpl fs;
     nit::Blob blob = fs.getBlobFromFile(bee_movie_path);
     auto r = nit::hashBlob(blob);
     EXPECT_EQ(r, "93ae3d6436613af8a6957db81e1701fbc50de7a8");
+}
+
+TEST(ObjectHashTests, TestHashToFSPath) {
+    const std::filesystem::path objectStorePath("/Users/snoopy/code/doghouse/.nit/objects");
+    const std::string hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709";
+    const std::filesystem::path expected = objectStorePath / hash.substr(0, 2) / hash.substr(2);
+    const std::filesystem::path gotPath = nit::hashToFSPath(objectStorePath, hash);
+    EXPECT_EQ(gotPath, expected);
+}
+
+TEST(ObjectHashTests, TestPutObject) {
+    MockFileSystemAdaptor mockFs;
+    const std::string data = "hello world";
+    const nit::Blob blob(data.begin(), data.end());
+
+    const std::filesystem::path objectStorePath("/Users/snoopy/code/doghouse/.nit/objects");
+    const std::string hash = "2aae6c35c94fcfb415dbe95f408b9ce91ee846ed";
+    const std::filesystem::path expectedObjectPath = objectStorePath / hash.substr(0, 2) / hash.substr(2);
+
+    auto gotPath = nit::putObject(mockFs, objectStorePath, blob);
+    EXPECT_EQ(gotPath, expectedObjectPath);
+
+    auto gotBlob = mockFs.getBlobFromFile(gotPath);
+    EXPECT_EQ(blob, gotBlob);
 }
