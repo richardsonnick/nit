2de04edebeb77105470d276bb43c857216760a11
NickR23 <nic.rr@icloud.com>
2025-05-27 10:26:25 -0400

    snakecase

--- src/hash-object.h
+++ src/hash-object.h
@@ -1,6 +1,6 @@
 #include <hash/sha1.h>
 
-static std::string hashObject(std::string& path) {
+static std::string hash_object(std::string& path) {
     std::string s = "abc";
     return hash::sha1::hash_string(s);
 }
