371755a29b2eab9fcea93cfe8a5818389f559f26
NickR23 <nic.rr@icloud.com>
2025-05-28 13:32:37 -0400

    Add Tree

---
+++ src/Tree.h
@@ -0,0 +1,19 @@
+#include <vector>
+
+
+namespace nit {
+/** 
+ * Trees represent a snapshot of a tree of directories.
+ */
+class Tree {
+public:
+    Tree(const std::string& name, const std::string& mode, const std::string hash) : name(name), mode(mode), hash(hash) {};
+private:
+    std::string name;
+    std::string mode; // Modes: "100644" (normal file), "40000" (directory)
+    std::string hash;
+
+    std::vector<std::unique_ptr<Tree>> children;
+};
+
+}
