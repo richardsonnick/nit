eb51adfc5a373bebb1b5638158f8d1229f2c93e1
NickR23 <nic.rr@icloud.com>
2025-05-28 11:06:20 -0400

    Add decomposeHash()

--- src/hash-object.h
+++ src/hash-object.h
@@ -6,6 +6,12 @@
 
 namespace nit {
 
+static std::pair<std::string, std::string> decomposeHash(const std::string hash) {
+    const std::string prefix = hash.substr(0, 2);
+    const std::string suffix = hash.substr(2);
+    return std::make_pair(prefix, suffix);
+}
+
 static std::string hashBlob(const nit::Blob& blob) {
     std::string data(reinterpret_cast<const char*>(blob.data()), blob.size());
     std::istringstream iss(data);
@@ -13,8 +19,7 @@ static std::string hashBlob(const nit::Blob& blob) {
 }
 
 static std::filesystem::path hashToFSPath(const std::filesystem::path& objectStorePath, const std::string& hash) {
-    const std::string prefix = hash.substr(0, 2);
-    const std::string suffix = hash.substr(2);
+    auto [prefix, suffix] = decomposeHash(hash);
     return objectStorePath / prefix / suffix;
 }
 
