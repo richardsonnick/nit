d9c042920738f69ad95f012ef38b8a9815270909
NickR23 <nic.rr@icloud.com>
2025-05-29 10:49:59 -0400

    Tree.h overhaul

--- src/Tree.h
+++ src/Tree.h
@@ -1,19 +1,87 @@
 #include <vector>
 
+#pragma once
 
 namespace nit {
+
+std::string hashObject(const nit::Blob& obj);
+
+constexpr std::string DIRMODE = "40000";
+constexpr std::string FILEMODE = "100644";
+
+
+struct TreeEntry {
+    std::string name;
+    std::string mode;  // DIRMODE or FILEMODE. TODO maybe make this an enum?
+    std::string hash; // blob hash for files and tree hash for directories
+};
+
 /** 
  * Trees represent a snapshot of a tree of directories.
  */
 class Tree {
 public:
-    Tree(const std::string& name, const std::string& mode, const std::string hash) : name(name), mode(mode), hash(hash) {};
-private:
-    std::string name;
-    std::string mode; // Modes: "100644" (normal file), "40000" (directory)
-    std::string hash;
+    Tree() = default;
 
-    std::vector<std::unique_ptr<Tree>> children;
+    void addEntry(const TreeEntry& entry) {
+        entries.push_back(entry);
+        updateHash();
+    }
+
+    // Returns the serialization of the Tree's contents
+    Blob serialize() const {
+        Blob data;
+        for (const auto& entry : entries) {
+            std::string line = entry.mode + " " + entry.name;
+            data.insert(data.end(), line.begin(), line.end());
+            data.push_back('\0');
+            data.insert(data.end(), entry.hash.begin(), entry.hash.end());
+        }
+        return data;
+    }
+
+    void updateHash() {
+        hash = nit::hashObject(serialize());
+    }
+
+    static Tree deserialize(const Blob& data) {
+        Tree tree;
+        size_t pos = 0, dataSize = data.size();
+        while (pos < dataSize) {
+            size_t spacePos = pos;
+            while (data[spacePos] != ' ') spacePos++;
+            std::string name(data.begin() + pos, data.begin() + spacePos);
+            pos = spacePos + 1;
+
+            size_t nullPos = pos;
+            while (data[nullPos] != '\0') nullPos++;
+            std::string mode(data.begin() + pos, data.begin() + nullPos);
+            pos = nullPos + 1;
+
+            std::string hash(data.begin() + pos, data.begin() + pos + 40);
+            pos += 40;
+
+            tree.entries.push_back({mode, name, hash});
+        }
+
+        // I'm choosing not to update the tree's hash.
+        // This is unneccessary since the owning commit will have
+        // that information. To my knowledge this is how OG git works.
+        // tree.updateHash();
+        return tree;
+    }
+
+    const std::vector<TreeEntry>& getEntries() const {
+        return entries;
+    }
+
+    const std::string& getHash() const {
+        return hash;
+    }
+
+private:
+    std::vector<TreeEntry> entries;
+    std::string hash;
 };
 
 }
---
+++ test/TreeTest.cpp
@@ -0,0 +1,63 @@
+#include <gtest/gtest.h>
+#include <Tree.h>
+
+using namespace nit;
+
+class TreeTest : public ::testing::Test {
+protected:
+    Tree* tree;
+
+    void SetUp() override {
+        tree = new Tree();
+    }
+
+    void TearDown() override {
+        delete tree;
+    }
+};
+
+TEST_F(TreeTest, TestAddEntryAndHashChange) {
+    EXPECT_TRUE(tree->getEntries().empty());
+    auto initialHash = tree->getHash();
+
+    TreeEntry fileEntry{ "file1.txt", FILEMODE, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" };
+    tree->addEntry(fileEntry);
+    ASSERT_EQ(tree->getEntries().size(), (unsigned long)1);
+    EXPECT_NE(tree->getHash(), initialHash);
+
+    auto hashAfterFirstAdd = tree->getHash();
+    TreeEntry dirEntry{ "subdir", DIRMODE, "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" };
+    tree->addEntry(dirEntry);
+    EXPECT_EQ(tree->getEntries().size(), (unsigned long)2);
+    EXPECT_NE(tree->getHash(), hashAfterFirstAdd);
+}
+
+TEST_F(TreeTest, TestSerializeDeserialize) {
+    TreeEntry fileEntry{ "file1.txt", FILEMODE, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" };
+    TreeEntry dirEntry{ "subdir", DIRMODE, "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" };
+    tree->addEntry(fileEntry);
+    tree->addEntry(dirEntry);
+
+    auto serialized = tree->serialize();
+    Tree deserialized = Tree::deserialize(serialized);
+
+    const auto& originalEntries = tree->getEntries();
+    const auto& deserializedEntries = deserialized.getEntries();
+    ASSERT_EQ(originalEntries.size(), deserializedEntries.size());
+
+    for (size_t i = 0; i < originalEntries.size(); ++i) {
+        EXPECT_EQ(originalEntries[i].mode, deserializedEntries[i].mode);
+        EXPECT_EQ(originalEntries[i].name, deserializedEntries[i].name);
+        EXPECT_EQ(originalEntries[i].hash, deserializedEntries[i].hash);
+    }
+
+    // I'm choosing not to update the tree's hash.
+    // This is unneccessary since the owning commit will have
+    // that information. To my knowledge this is how OG git works.
+    // EXPECT_EQ(tree->getHash(), deserialized.getHash());
+}
+
+TEST_F(TreeTest, TestEmptyTreeHashable) {
+    Tree emptyTree;
+    EXPECT_EQ(tree->getHash(), emptyTree.getHash());
+}
