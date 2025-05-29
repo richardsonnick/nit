a742b2902b9e305649f44e551eb1d03412fd620c
NickR23 <nic.rr@icloud.com>
2025-05-29 11:52:52 -0400

    WIP refactor Commit.

---
+++ src/Commit.cpp
@@ -0,0 +1,165 @@
+#include <vector>
+#include <hash-object.h>
+#include <types.h>
+#include <Tree.h>
+
+#include <Commit.h>
+
+namespace fs = std::filesystem;
+
+namespace nit {
+
+//std::string hashObject(const nit::Blob& obj);
+
+std::string committerToString(const Committer& c) {
+    return c.name + " <" + c.email + "> " + c.timestamp + " " + c.timezone;
+}
+
+/** 
+ * Commits represent metadata for Trees.
+ * They basically tell us how we got to this state of the repo
+ */
+class Commit {
+    bool operator==(const Commit& other) const {
+        auto committerEqual = [](Committer c, Committer o) {
+            return c.name == o.name &&
+                c.email == o.email &&
+                c.timestamp == o.timestamp &&
+                c.timezone == o.timezone;
+        };
+        return treeHash == other.treeHash && 
+                parentHash == other.parentHash && 
+                author == other.author &&
+                committerEqual(this->committer, other.committer) &&
+                commitMessage == other.commitMessage &&
+                hash == other.hash;
+    }
+
+    /**
+     * Returns a "blank" commit with only the treeHash.
+     * Caller is responsible for filling out the rest of the commit
+     * and updating it's hash before "committing".
+     */
+    static Commit fromTree(const Tree& tree) {
+        auto treeHash = tree.getHash(); // TODO we should have a notion for a "stale" hash
+        return Commit(treeHash);
+    }
+
+    static std::string header(const Commit& c) {
+        std::ostringstream header;
+        header << "tree " << c.treeHash << "\n";
+        if (!c.parentHash.empty()) {
+            header << "parent " << c.parentHash << "\n";
+        }
+        header << "author " << c.author << "\n";
+        header << "committer " << committerToString(c.committer) << "\n";
+        header << "\n"; // blank line before message
+        return header.str();
+    }
+
+    // Returns the serialization of the Commit's contents
+    Blob serialize() const {
+        auto content = Commit::header(*this) + commitMessage;
+
+        std::ostringstream headerStream;
+        headerStream << "commit " << content.size() << '\0';
+        std::string header = headerStream.str();
+
+        Blob data;
+        data.insert(data.end(), header.begin(), header.end());
+        data.insert(data.end(), content.begin(), content.end());
+
+        return data;
+    }
+
+    static Commit deserialize(const Blob& data) {
+        Commit commit;
+        size_t pos = 0;
+        const size_t dataSize = data.size();
+
+        // Read until the first '\0' (skip the "commit <size>\0" header)
+        while (pos < dataSize && data[pos] != '\0') {
+            pos++;
+        }
+        if (pos == dataSize) throw std::runtime_error("Invalid commit serialization (missing header terminator)");
+        pos++; // skip '\0'
+
+        // Now parse lines until an empty line (message separator)
+        while (pos < dataSize) {
+            size_t lineEnd = pos;
+            while (lineEnd < dataSize && data[lineEnd] != '\n') lineEnd++;
+            if (lineEnd == pos) {
+                // Empty line means message starts after this
+                pos++; 
+                break;
+            }
+            std::string line(reinterpret_cast<const char*>(&data[pos]), lineEnd - pos);
+
+            if (line.starts_with("tree ")) {
+                commit.treeHash = line.substr(5);
+            } else if (line.starts_with("parent ")) {
+                commit.parentHash = line.substr(7);
+            } else if (line.starts_with("author ")) {
+                commit.author = line.substr(7);
+            } else if (line.starts_with("committer ")) {
+                std::string committerLine = line.substr(10);
+                std::istringstream ss(committerLine);
+                ss >> commit.committer.name;
+
+                // read until we hit a '<'
+                std::string token;
+                while (ss >> token) {
+                    if (token.starts_with("<")) {
+                        commit.committer.email = token.substr(1, token.size() - 2); // strip < and >
+                        break;
+                    } else {
+                        commit.committer.name += " " + token;
+                    }
+                }
+                ss >> commit.committer.timestamp;
+                ss >> commit.committer.timezone;
+            }
+
+            pos = lineEnd + 1;
+        }
+
+        // Read the commit message
+        if (pos < dataSize) {
+            commit.commitMessage = std::string(reinterpret_cast<const char*>(&data[pos]), dataSize - pos);
+        }
+
+        commit.updateHash();
+
+        return commit;
+    }
+
+    void updateHash() {
+        Blob serialization = serialize();
+        hash = nit::hashObject(serialization);
+    }
+
+    void addParent(Commit& parent) {
+        parent.updateHash(); // TODO rethink eager updates of hashes
+        parentHash = parent.getHash();
+        //Update this hash since we added the parent
+        updateHash();
+    }
+
+    std::string getHash() const {
+        return hash;
+    }
+
+    std::string getAuthor() const {
+        return author;
+    }
+
+    std::string getParentHash() const {
+        return parentHash;
+    }
+
+    std::string getTreeHash() const {
+        return treeHash;
+    }
+};
+
+}
