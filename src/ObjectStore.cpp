8bb36222b9166a71247259d2f04fd9c110462134
NickR23 <nic.rr@icloud.com>
2025-05-29 12:41:26 -0400

    Refactor ObjectStore

---
+++ src/ObjectStore.cpp
@@ -0,0 +1,20 @@
+#include <hash-object.h>
+
+#include <ObjectStore.h>
+
+namespace nit {
+
+std::filesystem::path ObjectStore::putObject(const nit::Blob& blob) {
+    const std::string hash = hashObject(blob);
+    const std::filesystem::path path = hashToFSPath(objectStorePath, hash);
+    fs->writeBlobToFile(path, blob);
+    return path;
+}
+
+Blob ObjectStore::getObject(const std::string hash) {
+    auto [prefix, suffix] = decomposeHash(hash);
+    const std::filesystem::path path = objectStorePath / prefix / suffix;
+    return fs->getBlobFromFile(path);
+}
+
+} // namespace nit
--- src/ObjectStore.h
+++ src/ObjectStore.h
@@ -1,7 +1,6 @@
-#include <hash/sha1.h>
-#include <FileSystemAdaptor.h>
 #include <filesystem>
-#include <hash-object.h>
+
+#include <FileSystemAdaptor.h>
 
 namespace nit {
 
@@ -9,18 +8,8 @@ class ObjectStore {
     public:
     ObjectStore(const std::filesystem::path& objectStorePath,  std::shared_ptr<FileSystemAdaptorInterface> fs) : objectStorePath(objectStorePath), fs(fs) {};
 
-    std::filesystem::path putObject(const nit::Blob& blob) {
-        const std::string hash = hashObject(blob);
-        const std::filesystem::path path = hashToFSPath(objectStorePath, hash);
-        fs->writeBlobToFile(path, blob);
-        return path;
-    }
-
-    Blob getObject(const std::string hash) {
-        auto [prefix, suffix] = decomposeHash(hash);
-        const std::filesystem::path path = objectStorePath / prefix / suffix;
-        return fs->getBlobFromFile(path);
-    }
+    std::filesystem::path putObject(const nit::Blob& blob);
+    Blob getObject(const std::string hash);
 
     private:
     const std::filesystem::path& objectStorePath;
