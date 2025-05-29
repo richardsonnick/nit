979869f6f5fa4cd6687778ccb0735d12f064f0bf
NickR23 <nic.rr@icloud.com>
2025-05-29 12:39:07 -0400

    Refactor Tree

---
+++ src/Tree.cpp
@@ -0,0 +1,64 @@
+#include <hash-object.h>
+
+#include <Tree.h>
+
+namespace nit {
+
+
+void Tree::addEntry(const TreeEntry& entry) {
+    entries.push_back(entry);
+    updateHash();
+}
+
+// Returns the serialization of the Tree's contents
+Blob Tree::serialize() const {
+    Blob data;
+    for (const auto& entry : entries) {
+        std::string line = entry.mode + " " + entry.name;
+        data.insert(data.end(), line.begin(), line.end());
+        data.push_back('\0');
+        data.insert(data.end(), entry.hash.begin(), entry.hash.end());
+    }
+    return data;
+}
+
+void Tree::updateHash() {
+    hash = nit::hashObject(serialize());
+}
+
+Tree Tree::deserialize(const Blob& data) {
+    Tree tree;
+    size_t pos = 0, dataSize = data.size();
+    while (pos < dataSize) {
+        size_t spacePos = pos;
+        while (data[spacePos] != ' ') spacePos++;
+        std::string name(data.begin() + pos, data.begin() + spacePos);
+        pos = spacePos + 1;
+
+        size_t nullPos = pos;
+        while (data[nullPos] != '\0') nullPos++;
+        std::string mode(data.begin() + pos, data.begin() + nullPos);
+        pos = nullPos + 1;
+
+        std::string hash(data.begin() + pos, data.begin() + pos + 40);
+        pos += 40;
+
+        tree.entries.push_back({mode, name, hash});
+    }
+
+    // I'm choosing not to update the tree's hash.
+    // This is unneccessary since the owning commit will have
+    // that information. To my knowledge this is how OG git works.
+    // tree.updateHash();
+    return tree;
+}
+
+const std::vector<TreeEntry>& Tree::getEntries() const {
+    return entries;
+}
+
+const std::string& Tree::getHash() const {
+    return hash;
+}
+
+} // namespace nit
--- src/Tree.h
+++ src/Tree.h
@@ -1,17 +1,13 @@
 #include <vector>
 
 #include <types.h>
-#include <hash-object.h>
 
 #pragma once
 
 namespace nit {
 
-std::string hashObject(const nit::Blob& obj);
-
 constexpr std::string DIRMODE = "40000";
 constexpr std::string FILEMODE = "100644";
-
 
 struct TreeEntry {
     std::string name;
@@ -26,61 +22,13 @@ class Tree {
 public:
     Tree() = default;
 
-    void addEntry(const TreeEntry& entry) {
-        entries.push_back(entry);
-        updateHash();
-    }
-
+    void addEntry(const TreeEntry& entry);
     // Returns the serialization of the Tree's contents
-    Blob serialize() const {
-        Blob data;
-        for (const auto& entry : entries) {
-            std::string line = entry.mode + " " + entry.name;
-            data.insert(data.end(), line.begin(), line.end());
-            data.push_back('\0');
-            data.insert(data.end(), entry.hash.begin(), entry.hash.end());
-        }
-        return data;
-    }
-
-    void updateHash() {
-        hash = nit::hashObject(serialize());
-    }
-
-    static Tree deserialize(const Blob& data) {
-        Tree tree;
-        size_t pos = 0, dataSize = data.size();
-        while (pos < dataSize) {
-            size_t spacePos = pos;
-            while (data[spacePos] != ' ') spacePos++;
-            std::string name(data.begin() + pos, data.begin() + spacePos);
-            pos = spacePos + 1;
-
-            size_t nullPos = pos;
-            while (data[nullPos] != '\0') nullPos++;
-            std::string mode(data.begin() + pos, data.begin() + nullPos);
-            pos = nullPos + 1;
-
-            std::string hash(data.begin() + pos, data.begin() + pos + 40);
-            pos += 40;
-
-            tree.entries.push_back({mode, name, hash});
-        }
-
-        // I'm choosing not to update the tree's hash.
-        // This is unneccessary since the owning commit will have
-        // that information. To my knowledge this is how OG git works.
-        // tree.updateHash();
-        return tree;
-    }
-
-    const std::vector<TreeEntry>& getEntries() const {
-        return entries;
-    }
-
-    const std::string& getHash() const {
-        return hash;
-    }
+    Blob serialize() const;
+    void updateHash();
+    static Tree deserialize(const Blob& data);
+    const std::vector<TreeEntry>& getEntries() const;
+    const std::string& getHash() const;
 
 private:
     std::vector<TreeEntry> entries;
