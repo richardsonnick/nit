67595e3d19037d487a2ca665ada59b7c91f3c632
NickR23 <nic.rr@icloud.com>
2025-06-25 14:36:31 -0400

    Fix createInitRepo() test func

--- test/utils/TestUtils.h
+++ test/utils/TestUtils.h
@@ -12,11 +12,27 @@ std::string createInitialRepo(std::shared_ptr<FileSystemAdaptorInterface> fsa) {
     std::filesystem::path repoPath = "/home/snoopy/code/doghouse";
     std::filesystem::path srcPath = repoPath / "src";
     std::filesystem::path nitPath = repoPath / ".nit";
+    std::filesystem::path objectsPath = nitPath / "objects";
     std::filesystem::path includePath = repoPath / "include";
     std::filesystem::path mainCppPath = srcPath / "main.cpp";
+    std::filesystem::path utilsCppPath = srcPath / "utils.cpp";
     std::filesystem::path headerPath = includePath / "header.h";
+    std::filesystem::path utilsHeaderPath = includePath / "utilsHeader.h";
 
-    fsa->createDirectory(srcPath);
+    fsa->addEntry(repoPath, {});
+    fsa->setMetadata(repoPath, {
+        .ctime = 1640995100,
+        .mtime = 1640995090,
+        .deviceID = 2049,
+        .inode = 123436,
+        .fileMode = 0040755,
+        .userId = 1000,
+        .groupId = 1000,
+        .fileSize = 8096,
+        .flags = 0,
+        .pathName = repoPath.string()
+    });
+    fsa->addEntry(srcPath, {});
     fsa->setMetadata(srcPath, {
         .ctime = 1640995200,
         .mtime = 1640995200,
@@ -29,7 +45,7 @@ std::string createInitialRepo(std::shared_ptr<FileSystemAdaptorInterface> fsa) {
         .flags = 0,
         .pathName = srcPath.string()
     });
-    fsa->createDirectory(includePath);
+    fsa->addEntry(includePath, {});
     fsa->setMetadata(includePath, {
         .ctime = 1640995211,
         .mtime = 1640995400,
@@ -40,9 +56,9 @@ std::string createInitialRepo(std::shared_ptr<FileSystemAdaptorInterface> fsa) {
         .groupId = 1000,
         .fileSize = 2048,
         .flags = 0,
-        .pathName = srcPath.string()
+        .pathName = includePath.string()
     });
-    fsa->createDirectory(nitPath);
+    fsa->addEntry(nitPath, {});
     fsa->setMetadata(nitPath, {
         .ctime = 1640995811,
         .mtime = 1640997400,
@@ -53,7 +69,7 @@ std::string createInitialRepo(std::shared_ptr<FileSystemAdaptorInterface> fsa) {
         .groupId = 1000,
         .fileSize = 4096,
         .flags = 0,
-        .pathName = srcPath.string()
+        .pathName = nitPath.string()
     });
 
     std::string mainCppContentStr = R"(#include <iostream>
@@ -62,11 +78,21 @@ std::cout << "Hello World!" << std::endl;
 return 0;
 })";
     std::vector<uint8_t> mainCppContent(mainCppContentStr.begin(), mainCppContentStr.end());
-
     std::string headerContentStr = R"(#pragma once
 void function();
 )";
     std::vector<uint8_t> headerContent(headerContentStr.begin(), headerContentStr.end());
+
+    std::string utilsCppContentStr = R"(#include <iostream>
+int ShoutItOut() {
+std::cout << "Scream and SHOUT!!!" << std::endl;
+return 0;
+})";
+    std::vector<uint8_t> utilsCppContent(utilsCppContentStr.begin(), utilsCppContentStr.end());
+    std::string utilsHeaderContentStr = R"(#pragma once
+void utility();
+)";
+    std::vector<uint8_t> utilsHeaderContent(utilsHeaderContentStr.begin(), utilsHeaderContentStr.end());
 
     fsa->writeBlobToFile(mainCppPath, mainCppContent);
     fsa->setMetadata(mainCppPath, {
@@ -93,6 +119,33 @@ void function();
         .fileSize = 1024,
         .flags = 0,
         .pathName = headerPath.string()
+    });
+
+    fsa->writeBlobToFile(utilsCppPath, utilsCppContent);
+    fsa->setMetadata(utilsCppPath, {
+        .ctime = 1640995200,
+        .mtime = 1640995300,
+        .deviceID = 2049,
+        .inode = 789012,
+        .fileMode = 0100644,
+        .userId = 1000,
+        .groupId = 1000,
+        .fileSize = 2048,
+        .flags = 0,
+        .pathName = utilsCppPath.string()
+    });
+    fsa->writeBlobToFile(utilsHeaderPath, utilsHeaderContent);
+    fsa->setMetadata(utilsHeaderPath, {
+        .ctime = 1640995400,
+        .mtime = 1640995500,
+        .deviceID = 2049,
+        .inode = 345678,
+        .fileMode = 0100755,
+        .userId = 1000,
+        .groupId = 1000,
+        .fileSize = 1024,
+        .flags = 0,
+        .pathName = utilsHeaderPath.string()
     });
 
     return repoPath;
